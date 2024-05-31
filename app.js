35235df890c4150428694fa596cb14a4697ff57e
Zedmous <ejnsilva29@gmail.com>
2024-05-30 21:01:50 -0400

    resta, multi, division

--- app.js
+++ app.js
@@ -7,3 +7,11 @@ let calculadora= new Calculadora();
 
 calculadora.suma(2,3)
 console.log("suma: ",calculadora.suma(2,3))
+
+console.log("resta: ",calculadora.resta(2,3))
+
+console.log("multiplicacion: ",calculadora.multi(2,3))
+
+console.log("division: ",calculadora.division(6,3))
+
+console.log("division con 0: ",calculadora.division(6,0))
--- clase/calculadora.js
+++ clase/calculadora.js
@@ -12,7 +12,20 @@ class Calculadora{
         
         return a+b 
     }
+    resta= (a,b) => {
+        
+        return a-b 
+    }
+    multi= (a,b) => {
+        
+        return a*b 
+    }
 
+    division= (a,b) => {
+        if(b!=0)
+            return a/b
+        return 0
+    }
 }
 //
 module.exports={
